f5e4ddbe08412b6c45ea759b632bd87436fde09b
liamrahav <liorogamerdvd@gmail.com>
2015-05-29 08:55:43 -0400

    Added all base files

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+		grunt.initConfig({
+			sass: {
+				compile: {
+					options: {
+						style: 'compressed',
+						sourcemap: 'none'
+					},
+					files: {
+						'css/styles.css': 'css/sass/importer.scss'
+					}
+				}
+			},
+			
+			htmlmin: {
+				dist: {
+					options: {
+						removeComments: true,
+						collapseWhitespace:true
+					},
+					files: {
+						'index.html': '_index.html',
+					}
+				}
+			},
+			
+			watch: {
+				style: {
+					files: ["css/**/*.scss", '*.html'],
+					tasks: ['sass', 'htmlmin']
+				}
+			}
+		});
+		
+		grunt.loadNpmTasks('grunt-contrib-watch');
+		grunt.loadNpmTasks('grunt-contrib-sass');
+		grunt.loadNpmTasks('grunt-contrib-htmlmin');
+		
+		grunt.registerTask('default', ['sass:compile', 'htmlmin',"watch"]);
+};
